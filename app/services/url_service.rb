9c8a8ecda5f104ce3d14db9f6eb95fb3d23d68f9
Steve Wilson <swilson@mojoware.com>
2021-06-19 12:42:49 -0500

    Update url service.

--- app/services/url_service.rb
+++ app/services/url_service.rb
@@ -2,7 +2,7 @@ class UrlService
 
     def self.shorten(link)
         client = Bitly::API::Client.new(token: ENV['BITLY_TOKEN'])
-        bitlink = client.shorten(long_url: "http://example.com")
+        bitlink = client.shorten(long_url: link)
         bitlink.link
     end
 
