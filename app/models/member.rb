0f9ce9cdf0e6e2619c2e40739cf04511d119337d
Steve Wilson <swilson@mojoware.com>
2021-06-20 08:22:15 -0500

    Add search service.

--- app/models/member.rb
+++ app/models/member.rb
@@ -17,4 +17,48 @@ class Member < ApplicationRecord
         friends
     end
 
+    def full_name
+        first_name + ' ' + last_name
+    end
+
+    def self.search(source, dest)
+        route = ''
+        nodes = {}
+        graph = Graph.new
+
+        # Loop through all members and create nodes
+        Member.all.each do |member|
+            node_key  =  "node_#{member.id}"
+            nodes.merge!(node_key => Node.new(member.full_name))
+        end
+
+        # Loop through all friendships and create edges
+        Friendship.all.each do |friendship|
+            # Get the source and destination nodes
+            member_node = nodes["node_#{friendship.member_id}"]
+            friend_node = nodes["node_#{friendship.friend_id}"]
+            # Add the edge relation in the members graph
+            graph.add_edge(member_node, friend_node)
+        end
+
+        # Get source and dest nodes
+        source_node = nodes["node_#{source}"]
+        dest_node   = nodes["node_#{dest}"]
+
+        # Find shortest path between source and dest
+        path = SearchService.new(graph, source_node).shortest_path_to(dest_node)
+
+        # Create shortest path string result
+        path.each_with_index do |p, index|
+            if index == path.size - 1
+                route += p.name
+            else
+                route += p.name + ' -> '
+            end
+        end
+
+        route
+
+    end
+
 end
